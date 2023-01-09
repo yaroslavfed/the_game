6801c212168d286d7b1454a25320c5899d7f5bd8
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2023-01-10 02:27:55 +0700

    Добавлены страницы "О нас" и "Настройки"

---
+++ the_game/settings_page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace the_game
+{
+    /// <summary>
+    /// Логика взаимодействия для settings_page.xaml
+    /// </summary>
+    public partial class settings_page : Window
+    {
+        public settings_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
